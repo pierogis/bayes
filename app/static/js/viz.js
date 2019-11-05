fa0bad60c7bbb6e4b564ad9405e3a7ecc282a2c4
Kyle Moore <moore.kyle39@gmail.com>
2019-11-04 22:27:33 -0500

    add information with hover bars, polish

--- app/static/js/viz.js
+++ app/static/js/viz.js
@@ -19,7 +19,19 @@ window.onload = function() {
   var panels = [left, right]
   var total_space = 0
 
+  var hoverWait
+
+  _c = 1.55
+  _p = 10
   r = .001
+
+  function sleep(t) {
+    var now = new Date().getTime();
+
+    while (new Date().getTime() < now + t ) {
+
+    }
+  }
 
   function dragProbs(box1, box2) {
     total_space = 0
@@ -76,229 +88,8 @@ window.onload = function() {
     dragProbs(top_right, bottom_right)
   }
 
-  for (i = 0; i < boxes.length; i++) {
-    total_space += boxes[i].clientWidth * boxes[i].clientHeight
-  }
-
-  releaseProbs()
-
-  mid_bar.addEventListener('mousedown', (e) => {
-    barHold(mid_bar, e)
-  })
-
-  left_bar.addEventListener('mousedown', (e) => {
-    barHold(left_bar, e)
-  })
-
-  right_bar.addEventListener('mousedown', (e) => {
-    barHold(right_bar, e)
-  })
-
-  function barHold(bar, e) {
-    function pressingDown(e) {
-      cancelAnimationFrame(timerID);
-
-      f = r
-
-      if (bar == mid_bar) {
-        dragProbs(left_side[0], right_side[0])
-        left_side[0].children[0].style.display = "inline"
-        right_side[0].children[0].style.display = "inline"
-        left_side[0].children[1].style.display = "none"
-        right_side[0].children[1].style.display = "none"
-
-        document.addEventListener("mousemove", midListener, false)
-      }
-      else {
-        top_s.children[0].style.display = "inline"
-        top_s.children[1].style.display = "none"
-
-        bot_s.children[0].style.display = "inline"
-        bot_s.children[1].style.display = "none"
-
-        side_o[0].children[1].style.display = "inline"
-
-        dragProbs(top_s, bot_s)
-        if (bar == left_bar) {
-          document.addEventListener("mousemove", leftListener, false)
-        }
-        else if (bar == right_bar) {
-          document.addEventListener("mousemove", rightListener, false)
-        }
-      }
-
-      e.preventDefault();
-
-      requestAnimationFrame(holdTimer);
-    }
-
-    function notPressingDown(e) {
-      cancelAnimationFrame(timerID);
-
-      r = f
-
-      if (bar == mid_bar) {
-        document.removeEventListener("mousemove", midListener, false)
-      }
-      else {
-        top_s.children[0].style.display = null
-        top_s.children[1].style.display = null
-
-        bot_s.children[0].style.display = null
-        bot_s.children[1].style.display = null
-        if (bar == left_bar) {
-          document.removeEventListener("mousemove", leftListener, false)
-        }
-        else if (bar == right_bar) {
-          document.removeEventListener("mousemove", rightListener, false)
-        }
-      }
-
-      for (i = 1; i < left_side.length; i++) {
-        if (bar == mid_bar) {
-          left_side[i].style.opacity = 1
-          right_side[i].style.opacity = 1
-        }
-        else {
-          side_o[i].style.opacity = 1
-        }
-      }
-
-      bar.removeEventListener("mouseup", notPressingDown, false)
-
-      requestAnimationFrame(releaseTimer);
-
-      releaseProbs()
-    }
-
-    function holdTimer() {
-      if (f < 300) {
-        timerID = requestAnimationFrame(holdTimer);
-        f = f*1.55
-        console.log(f)
-
-        if (bar == mid_bar) {
-          left_side[0].style.flex = f
-          right_side[0].style.flex = f
-          left_bar.style.padding = 8 * (1 - f / 10) + "px"
-          right_bar.style.padding = 8 * (1 - f / 10) + "px"
-
-          left_side[0].children[0].style.opacity = (1 - 1/f / 5)
-          right_side[0].children[0].style.opacity = (1 - 1/f / 5)
-
-          left_side[1].children[1].style.opacity = (1 - f / 5)
-          left_side[3].children[1].style.opacity = (1 - f / 5)
-          right_side[1].children[1].style.opacity = (1 - f / 5)
-          right_side[3].children[1].style.opacity = (1 - f / 5)
-
-          dragProbs(left_side[0], right_side[0])
-        }
-        else {
-          panel_o.style.flex = f
-          bar_o.style.padding = 8 * (1 - f / 10) + "px"
-
-          side_o[0].children[1].style.opacity = (1 - 1/f / 5)
-
-          top_o.children[1].style.opacity = (1 - f / 5)
-          bot_o.children[1].style.opacity = (1 - f / 5)
-          dragProbs(top_s, bot_s)
-        }
-      }
-      else {
-        for (i = 1; i < left_side.length; i++) {
-          if (bar == mid_bar) {
-            left_side[i].style.opacity = 0
-            right_side[i].style.opacity = 0
-          }
-          else {
-            side_o[i].style.opacity = 0
-          }
-        }
-        if (bar == mid_bar) {
-          left_side[0].style.flex = 10000
-          right_side[0].style.flex = 10000
-          left_bar.style.padding = 0
-          right_bar.style.padding = 0
-
-          left_side[0].children[0].style.display = null
-          right_side[0].children[0].style.display = null
-
-          dragProbs(left_side[0], right_side[0])
-        }
-        else {
-          panel_o.style.flex = 10000
-          bar_o.style.padding = 0
-
-          left_side[0].children[0].style.display = "none"
-          right_side[0].children[0].style.display = "none"
-
-          dragProbs(top_s, bot_s)
-        }
-        f = 300
-      }
-    }
-
-    function releaseTimer() {
-      if (r > 0.001) {
-        timerID = requestAnimationFrame(releaseTimer);
-        r = r/1.55
-
-        if (bar == mid_bar) {
-          left_side[0].style.flex = r
-          right_side[0].style.flex = r
-          left_bar.style.padding = 8 * (1 - r / 10) + "px"
-          right_bar.style.padding = 8 * (1 - r / 10) + "px"
-
-          left_side[0].children[0].style.opacity = (1 - 1/r / 5)
-          right_side[0].children[0].style.opacity = (1 - 1/r / 5)
-
-          left_side[1].children[1].style.opacity = (1 - r / 5)
-          left_side[3].children[1].style.opacity = (1 - r / 5)
-          right_side[1].children[1].style.opacity = (1 - r / 5)
-          right_side[3].children[1].style.opacity = (1 - r / 5)
-        }
-        else {
-          for (i = 1; i < left_side.length; i++) {
-            if (bar == mid_bar) {
-              left_side[i].style.opacity = 1
-              right_side[i].style.opacity = 1
-            }
-            else {
-              side_o[i].style.opacity = 1
-            }
-          }
-          panel_o.style.flex = r
-          bar_o.style.padding = 8 * (1 - r / 10) + "px"
-
-          side_o[0].children[1].style.opacity = (1 - 1/r / 5)
-
-          top_o.children[1].style.opacity = (1 - r / 5)
-          bot_o.children[1].style.opacity = (1 - r / 5)
-
-          releaseProbs()
-        }
-      }
-      else {
-        if (bar == mid_bar) {
-          left_side[0].style.flex = 0
-          right_side[0].style.flex = 0
-          left_bar.style.padding = "8px"
-          right_bar.style.padding = "8px"
-
-          left_side[0].children[0].style.display = "none"
-          right_side[0].children[0].style.display = "none"
-        }
-        else {
-          panel_o.style.flex = 0
-          bar_o.style.padding = "8px"
-        }
-        releaseProbs()
-        r = .001
-      }
-    }
-
-    // https://www.kirupa.com/html5/press_and_hold.htm
-    var timerID
+  function hovering(e) {
+    bar = e.target
 
     f = r
 
@@ -333,9 +124,274 @@ window.onload = function() {
       side_o = left_side
     }
 
-    bar.addEventListener("mouseup", notPressingDown, false)
-    // item.addEventListener("touchstart", pressingDown, false);
-    // item.addEventListener("touchend", notPressingDown, false);
-    pressingDown(e)
+    cancelAnimationFrame(timerID);
+
+    mid_bar.removeEventListener("mouseenter", hovering, false)
+    left_bar.removeEventListener("mouseenter", hovering, false)
+    right_bar.removeEventListener("mouseenter", hovering, false)
+
+    bar.addEventListener("mousedown", pressingDown)
+    bar.addEventListener("mouseleave", notHovering)
+
+    f = r
+
+    e.preventDefault();
+
+    hoverWait = setTimeout(function () {
+      if (bar == mid_bar){
+        left_side[0].children[0].style.display = "none"
+        left_side[0].children[1].style.display = "inline"
+        right_side[0].children[0].style.display = "none"
+        right_side[0].children[1].style.display = "inline"
+      }
+      else {
+        if (bar == left_bar) {
+          top_s.children[1].innerHTML = "P(B|A)"
+          bot_s.children[1].innerHTML = "P(!B|A)"
+        }
+        else {
+          top_s.children[1].innerHTML = "P(B|!A)"
+          bot_s.children[1].innerHTML = "P(!B|!A)"
+        }
+      }
+
+      requestAnimationFrame(holdTimer);
+    }, 100);
+
+
   }
+  function notHovering(e) {
+    cancelAnimationFrame(timerID);
+
+    clearTimeout(hoverWait)
+
+    bar.removeEventListener("mousedown", pressingDown)
+    bar.removeEventListener("mouseleave", notHovering)
+
+    bar.addEventListener("mouseenter", hovering)
+
+    r = f
+
+    for (i = 1; i < left_side.length; i++) {
+      if (bar == mid_bar) {
+        left_side[i].style.opacity = 1
+        right_side[i].style.opacity = 1
+      }
+      else {
+        side_o[i].style.opacity = 1
+      }
+    }
+
+    if (bar == left_bar) {
+      top_s.children[1].innerHTML = "P(A|B)"
+      bot_s.children[1].innerHTML = "P(A|!B)"
+    }
+    else if (bar == right_bar) {
+      top_s.children[1].innerHTML = "P(!A|B)"
+      bot_s.children[1].innerHTML = "P(!A|!B)"
+    }
+
+    requestAnimationFrame(releaseTimer);
+  }
+  function pressingDown(e) {
+    mid_bar.removeEventListener("mouseenter", hovering)
+    left_bar.removeEventListener("mouseenter", hovering)
+    right_bar.removeEventListener("mouseenter", hovering)
+
+    bar.addEventListener("mouseup", notPressingDown)
+    bar.removeEventListener("mousedown", pressingDown)
+    bar.removeEventListener("mouseleave", notHovering)
+
+    if (bar == mid_bar) {
+      // cross fade statement for prob
+      left_side[0].children[0].style.display = "inline"
+      left_side[0].children[1].style.display = "none"
+      right_side[0].children[0].style.display = "inline"
+      right_side[0].children[1].style.display = "none"
+      dragProbs(left_side[0], right_side[0])
+      document.addEventListener("mousemove", midListener)
+    }
+    else {
+      dragProbs(top_s, bot_s)
+      top_s.children[0].style.display = "inline"
+      top_s.children[1].style.display = "none"
+      bot_s.children[0].style.display = "inline"
+      bot_s.children[1].style.display = "none"
+      if (bar == left_bar) {
+        document.addEventListener("mousemove", leftListener)
+      }
+      else if (bar == right_bar) {
+        document.addEventListener("mousemove", rightListener)
+      }
+    }
+  }
+
+  function notPressingDown(e) {
+    bar.addEventListener("mousedown", pressingDown)
+
+    if (bar == mid_bar) {
+      left_side[0].children[0].style.display = "none"
+      left_side[0].children[1].style.display = "inline"
+      right_side[0].children[0].style.display = "none"
+      right_side[0].children[1].style.display = "inline"
+
+      document.removeEventListener("mousemove", midListener)
+    }
+    else {
+      top_s.children[0].style.display = "none"
+      top_s.children[1].style.display = "inline"
+      bot_s.children[0].style.display = "none"
+      bot_s.children[1].style.display = "inline"
+      if (bar == left_bar) {
+        top_s.children[1].innerHTML = "P(A|B)"
+        bot_s.children[1].innerHTML = "P(A|!B)"
+        document.removeEventListener("mousemove", leftListener)
+      }
+      else if (bar == right_bar) {
+        top_s.children[1].innerHTML = "P(!A|B)"
+        bot_s.children[1].innerHTML = "P(!A|!B)"
+        document.removeEventListener("mousemove", rightListener)
+      }
+    }
+
+    bar.addEventListener("mouseleave", notHovering)
+    bar.removeEventListener("mouseup", notPressingDown)
+  }
+
+  function holdTimer() {
+    if (f < 300) {
+
+      f = f*_c
+
+      if (bar == mid_bar) {
+        left_side[0].style.flex = f
+        right_side[0].style.flex = f
+        left_bar.style.padding = _p * (1 - f / 10) + "px"
+        right_bar.style.padding = _p * (1 - f / 10) + "px"
+
+        left_side[0].children[1].style.opacity = (1 - 1/f / 5)
+        right_side[0].children[1].style.opacity = (1 - 1/f / 5)
+
+        left_side[1].children[1].style.opacity = (1 - f / 5)
+        left_side[3].children[1].style.opacity = (1 - f / 5)
+        right_side[1].children[1].style.opacity = (1 - f / 5)
+        right_side[3].children[1].style.opacity = (1 - f / 5)
+
+        dragProbs(left_side[0], right_side[0])
+      }
+      else {
+        panel_o.style.flex = f
+        bar_o.style.padding = _p * (1 - f / 10) + "px"
+
+        side_o[0].children[1].style.opacity = (1 - 1/f / 5)
+
+        top_o.children[1].style.opacity = (1 - f / 5)
+        bot_o.children[1].style.opacity = (1 - f / 5)
+        dragProbs(top_s, bot_s)
+      }
+      timerID = requestAnimationFrame(holdTimer);
+    }
+    else {
+      for (i = 1; i < left_side.length; i++) {
+        if (bar == mid_bar) {
+          left_side[i].style.opacity = 0
+          right_side[i].style.opacity = 0
+        }
+        else {
+          side_o[i].style.opacity = 0
+        }
+      }
+      if (bar == mid_bar) {
+        left_side[0].style.flex = 10000
+        right_side[0].style.flex = 10000
+        left_bar.style.padding = 0
+        right_bar.style.padding = 0
+
+        dragProbs(left_side[0], right_side[0])
+      }
+      else {
+        panel_o.style.flex = 10000
+        bar_o.style.padding = 0
+
+        dragProbs(top_s, bot_s)
+      }
+      f = 300
+    }
+  }
+
+  function releaseTimer() {
+    if (r > 0.001) {
+
+      r = r/_c
+
+      if (bar == mid_bar) {
+        left_side[0].style.flex = r
+        right_side[0].style.flex = r
+        left_bar.style.padding = _p * (1 - r / 10) + "px"
+        right_bar.style.padding = _p * (1 - r / 10) + "px"
+
+        left_side[0].children[1].style.opacity = (1 - 1/r / 5)
+        right_side[0].children[1].style.opacity = (1 - 1/r / 5)
+
+        left_side[1].children[1].style.opacity = (1 - r / 5)
+        left_side[3].children[1].style.opacity = (1 - r / 5)
+        right_side[1].children[1].style.opacity = (1 - r / 5)
+        right_side[3].children[1].style.opacity = (1 - r / 5)
+      }
+      else {
+        panel_o.style.flex = r
+        bar_o.style.padding = _p * (1 - r / 10) + "px"
+
+        side_o[0].children[1].style.opacity = (1 - 1/r / 5)
+
+        top_o.children[1].style.opacity = (1 - r / 5)
+        bot_o.children[1].style.opacity = (1 - r / 5)
+
+        releaseProbs()
+      }
+      timerID = requestAnimationFrame(releaseTimer);
+    }
+    else {
+      for (i = 1; i < left_side.length; i++) {
+        if (bar == mid_bar) {
+          left_side[i].style.opacity = 1
+          right_side[i].style.opacity = 1
+        }
+        else {
+          side_o[i].style.opacity = 1
+        }
+      }
+      if (bar == mid_bar) {
+        left_side[0].style.flex = 0
+        right_side[0].style.flex = 0
+        left_bar.style.padding = _p + "px"
+        right_bar.style.padding = _p + "px"
+
+        left_bar.addEventListener("mouseenter", hovering)
+        right_bar.addEventListener("mouseenter", hovering)
+      }
+      else {
+        panel_o.style.flex = 0
+        bar_o.style.padding = _p + "px"
+
+        mid_bar.addEventListener("mouseenter", hovering)
+        bar_o.addEventListener("mouseenter", hovering)
+      }
+
+      releaseProbs()
+      r = .001
+    }
+  }
+
+  for (i = 0; i < boxes.length; i++) {
+    total_space += boxes[i].clientWidth * boxes[i].clientHeight
+  }
+
+  var timerID
+
+  releaseProbs()
+
+  mid_bar.addEventListener("mouseenter", hovering)
+  left_bar.addEventListener("mouseenter", hovering)
+  right_bar.addEventListener("mouseenter", hovering)
 }
